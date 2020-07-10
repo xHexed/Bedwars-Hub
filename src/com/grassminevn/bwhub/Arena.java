79e4ba63643aa74eb8278e89b2cfd44a9e551e64
BuildTools <unconfigured@null.spigotmc.org>
2020-07-10 21:34:24 +0700

    Fix debug message

--- src/com/grassminevn/bwhub/Arena.java
+++ src/com/grassminevn/bwhub/Arena.java
@@ -30,7 +30,7 @@ public class Arena {
 
     public void setStatus(final ArenaStatus status) {
         this.status = status;
-        System.out.println("Updated status to " + status.name());
+        System.out.println("Updated " + name + " status to " + status.name());
         Events.updateView();
     }
 
